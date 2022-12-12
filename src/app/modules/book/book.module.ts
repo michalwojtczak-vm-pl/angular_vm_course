2c7195c14dda4a0d450422233a2fd8685576fe94
DHodaniewicz <d.hodaniewicz@gmail.com>
2022-12-12 22:20:00 +0100

    Homework done

--- src/app/modules/book/book.module.ts
+++ src/app/modules/book/book.module.ts
@@ -4,6 +4,8 @@ import { BookComponent } from './book.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookCreateComponent } from './components/book-create/book-create.component';
 import { RouterModule, Routes } from '@angular/router';
+import {CategoryService} from "../category/services/category.service";
+import {BookService} from "./services/book.service";
 
 const routes: Routes = [
   {
@@ -37,6 +39,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
+  ],
+  providers: [
+    BookService
   ]
 })
 export class BookModule { }
--- src/app/modules/book/components/book-list/book-list.component.ts
+++ src/app/modules/book/components/book-list/book-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BookInterface} from "../../interfaces/book-interface";
+import {BookService} from "../../services/book.service";
 
 @Component({
   selector: 'app-book-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-list.component.scss']
 })
 export class BookListComponent implements OnInit {
+  books: BookInterface[] = [];
 
-  constructor() { }
+  constructor(private bookService : BookService) { }
 
   ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.loadData().subscribe((result) => {
+      this.books = result;
+    })
   }
 
 }
